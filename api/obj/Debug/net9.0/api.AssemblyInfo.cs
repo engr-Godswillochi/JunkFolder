a2a3ed2601aec29a48e04a6b39023e31d1d343cf
engr-Godswillochi <ochigodswill@gmail.com>
2025-02-27 02:16:14 +0100

    concluded

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e15f38cf3913cb196d7e7ec784b80be9cb34de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6f9f0980da708af997093c5fa90db6df6f721f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
